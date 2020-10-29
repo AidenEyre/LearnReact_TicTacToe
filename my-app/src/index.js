20490b52da6424f82a1ce278dfeb07d94eff98f1
AidenEyre <EyreAiden@gmail.com>
2020-10-29 16:29:44 -0600

    changed this.props to props.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 function Square(props) {
   return (
     <button className="square" onClick={() => this.props.onClick()}>
-      {this.props.value}
+      {props.value}
     </button>
   );
 }
